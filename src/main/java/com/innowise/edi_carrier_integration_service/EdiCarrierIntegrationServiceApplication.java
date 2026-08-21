94cb61de958fc24164042b8a57fc95f8282fa853
MrKravz <4otkiipozzik44@gmail.com>
2026-08-21 11:26:03 +0300

    ci: integrate shared CI pipeline and pre-commit hooks

--- src/main/java/com/innowise/edi_carrier_integration_service/EdiCarrierIntegrationServiceApplication.java
+++ src/main/java/com/innowise/edi_carrier_integration_service/EdiCarrierIntegrationServiceApplication.java
@@ -3,11 +3,19 @@ package com.innowise.edi_carrier_integration_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EdiCarrierIntegrationServiceApplication {
+@SpringBootApplication(proxyBeanMethods = false)
+public final class EdiCarrierIntegrationServiceApplication {
+  /**
+   * Main method to start the Spring Boot application.
+   *
+   * @param args command line arguments
+   */
+  public static void main(final String[] args) {
+    Class<?> appClass = EdiCarrierIntegrationServiceApplication.class;
+    SpringApplication.run(appClass, args);
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdiCarrierIntegrationServiceApplication.class, args);
-	}
-
+  private EdiCarrierIntegrationServiceApplication() {
+    super();
+  }
 }
