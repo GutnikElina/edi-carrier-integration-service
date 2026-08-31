c2de24f4369c385f915a52d32643841a44eb6c85
hilt0n <ddjj676dd@gmail>
2026-08-31 12:41:17 +0300

    R.S. Finished E02-task with unit-tests

--- src/test/java/com/innowise/edi_carrier_integration_service/CamelAs2ServerApplicationTests.java
+++ src/test/java/com/innowise/edi_carrier_integration_service/CamelAs2ServerApplicationTests.java
@@ -1,5 +1,8 @@
 package com.innowise.edi_carrier_integration_service;
 
+import com.innowise.edi_carrier_integration_service.edi.infrastructure.archive.EdiArchiveService;
+import com.innowise.edi_carrier_integration_service.edi.infrastructure.crypto.KeyManagementService;
+import io.minio.MinioClient;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
@@ -12,12 +15,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-/** Integration tests for AS2 inbound route. */
 @SpringBootTest
 @UseAdviceWith
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CamelAs2ServerApplicationTests {
+
+  @MockBean private KeyManagementService keyManagementService;
+  @MockBean private EdiArchiveService ediArchiveService;
+  @MockBean private MinioClient minioClient;
 
   @Autowired private CamelContext camelContext;
 
@@ -48,13 +55,13 @@ class CamelAs2ServerApplicationTests {
   void shouldReceiveEdiMessage() throws Exception {
     final String edi =
         """
-            ISA*00*          *00*          *ZZ*SENDER         *ZZ*RECEIVER       *260821*1000*U*00401*000000001*0*P*>~
-            GS*PO*SENDER*RECEIVER*20260821*1000*1*X*004010~
-            ST*850*0001~
-            SE*2*0001~
-            GE*1*1~
-            IEA*1*000000001~
-            """;
+                ISA*00*          *00*          *ZZ*SENDER         *ZZ*RECEIVER       *260821*1000*U*00401*000000001*0*P*>~
+                GS*PO*SENDER*RECEIVER*20260821*1000*1*X*004010~
+                ST*850*0001~
+                SE*2*0001~
+                GE*1*1~
+                IEA*1*000000001~
+                """;
 
     mockResult.expectedMessageCount(1);
     mockResult.expectedBodiesReceived(edi);
